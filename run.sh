64088c1c06b8b94a438fafeabd05df2b4fd1824b
Asaf <46109899+asafravid@users.noreply.github.com>
2022-06-28 16:19:12 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,29 +2,30 @@ make generate
 make manifests
 make install
 
-export IMG=docker.io/navadiaev/marvell-sriov-operator:latest
+export IMG=docker.io/aravidmarvell/marvell-sriov-operator:latest
 export NAMESPACE=sriov-network-operator
 
 cd config/manager
-kustomize edit set image controller=${IMG}
-kustomize edit set namespace "${NAMESPACE}"
+../../bin/kustomize edit set image controller=${IMG}
+../../bin/kustomize edit set namespace "${NAMESPACE}"
 cd ../../
 
 cd config/default
-kustomize edit set namespace "${NAMESPACE}"
+../../bin/kustomize edit set namespace "${NAMESPACE}"
 cd ../../
 
 #build and push sriov operator
-docker build -f Dockerfile -t "navadiaev/marvell-sriov-operator" .
-docker push navadiaev/marvell-sriov-operator:latest
+docker build -f Dockerfile -t "aravidmarvell/marvell-sriov-operator" .
+docker push aravidmarvell/marvell-sriov-operator:latest
 
 #build and push network config
-docker build -f Dockerfile.sriov-network-config-daemon -t "navadiaev/marvell-sriov-config" .
-docker push navadiaev/marvell-sriov-config:latest
+docker build -f Dockerfile.sriov-network-config-daemon -t "aravidmarvell/marvell-sriov-config" .
+docker push aravidmarvell/marvell-sriov-config:latest
 
 #build and push webhook
-docker build -f Dockerfile.webhook -t "navadiaev/marvell-sriov-webhook" .
-docker push navadiaev/marvell-sriov-webhook:latest
+docker build -f Dockerfile.webhook -t "aravidmarvell/marvell-sriov-webhook" .
+docker push aravidmarvell/marvell-sriov-webhook:latest
 
 #deploy to operator
+# for CentOS: one-time invokation raquired for "yum install skopeo --allowerasing"
 make deploy-setup
